792a864e10f54248da82a9ceab62a28028b0c17e
Lucas Gomes <101675693+DevLucasGomes@users.noreply.github.com>
2022-12-29 20:51:47 -0300

    att

--- Dupla de prova/index2.js
+++ Dupla de prova/index2.js
@@ -2,6 +2,7 @@ function solucao (notaAlunoA, notaAlunoB) {
     notaAlunoA = 8;
 	notaAlunoB = 6;
 	
+	console.log(notaAlunoA)
 }
 function naoMexer(input) {
 	const [notaAlunoA, notaAlunoB] = input.trim().split("\n").map(x => Number(x));
--- ex 007/index.js
+++ ex 007/index.js
@@ -3,5 +3,11 @@ let preçoProduto2 = 0.80;
 let preçoProduto3 = 4.85;
 
 if (preçoProduto1 <= 18) {
-    
+    console.log("Comprei o produto 1");
 }
+
+if (preçoProduto2 < 3) {
+   if (preçoProduto2 >= 1.5) {
+    console.log("comprei o produto 2");
+   }
+} 
