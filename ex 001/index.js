e1f575fef1830a8dd03ed5d573651cfca9ab12bd
Lucas Gomes <101675693+DevLucasGomes@users.noreply.github.com>
2022-12-29 19:26:18 -0300

    att

---
+++ ex 001/index.js
@@ -0,0 +1,18 @@
+// O console.log mostra uma mensagem
+console.log("Olá Mundo!");
+console.log("Como você está?");
+
+// Number = numero
+// String = texto
+// let = variavel
+// const = variável que não ira muda
+
+// variável etiqueta = "valor";
+let nome = "Lucas";
+
+const idade = 20;
+
+// Posso usar mais de uma vez essa variável
+console.log(nome);
+console.log(nome);
+console.log(idade);
---
+++ ex002/index.js
@@ -0,0 +1,8 @@
+const endereço = "Avenida 1";
+
+// nome da variável nao pode ter espaços
+let preçoDoLeite = 9.5;
+let preçoDoQueijo = 30;
+
+console.log("Preço do Leite:", preçoDoLeite);
+console.log("Preço do Queijo:", preçoDoQueijo)
